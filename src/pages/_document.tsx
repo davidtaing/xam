c6cab347d141b393d21caeb97c9ff2861539ba63
davidtaing <adavidtaing@gmail.com>
2023-03-06 21:56:39 +1100

    chore: remove original homepage and styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="prose max-w-full dark:prose-invert">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
