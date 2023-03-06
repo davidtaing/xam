8de4948df4de84bcc5ca0dc1a8cf6e7c2a244bae
davidtaing <adavidtaing@gmail.com>
2023-03-06 23:41:53 +1100

    feat: add Form logic to LoginForm

--- src/modules/Login/LoginForm.tsx
+++ src/modules/Login/LoginForm.tsx
@@ -1,31 +1,69 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import { users } from "../../../design samples/data/users_data";
+
+export const loginFormSchema = z.object({
+  branchId: z.coerce.number().int().positive(),
+  userName: z.string(),
+  password: z.string(),
+});
+
+export type LoginFormValues = z.infer<typeof loginFormSchema>;
+
 export function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  const onSubmit = (data: LoginFormValues) => {
+    users.forEach((user) => {
+      // contrived example, so I'm not too bothered
+      // with proper implementation here.
+      const branchIdMatch = data.branchId === user.branchId;
+      const userNameMatch = data.userName === user.userName;
+      const passwordMatch = data.password === user.password;
+
+      if (branchIdMatch && userNameMatch && passwordMatch) {
+        alert("Login successful");
+      } else {
+        alert("failed to login");
+      }
+    });
+  };
+
   return (
     <div
       aria-label="Login Panel"
       className="m-auto max-w-xl border border-slate-900 p-8"
     >
       <h1 className="text-2xl font-medium">Login</h1>
-      <form className="flex flex-col gap-8">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-8">
         <div className="flex flex-col gap-2">
           <label htmlFor="branch-id" className="text-sm">
             Branch id
           </label>
           <input
             id="branch-id"
-            name="branch-id"
-            className="rounded-md border border-slate-900 px-4 py-2"
             type="text"
+            {...register("branchId")}
+            className="rounded-md border border-slate-900 px-4 py-2"
           />
         </div>
         <div className="flex flex-col gap-2">
-          <label htmlFor="username" className="text-sm">
+          <label htmlFor="userName" className="text-sm">
             Username
           </label>
           <input
-            id="username"
-            name="username"
+            id="userName"
+            type="text"
+            {...register("userName")}
             className="rounded-md border border-slate-900 px-4 py-2"
-            type="username"
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -34,9 +72,9 @@ export function LoginForm() {
           </label>
           <input
             id="password"
-            name="password"
-            className="rounded-md border border-slate-900 px-4 py-2"
             type="password"
+            {...register("password")}
+            className="rounded-md border border-slate-900 px-4 py-2"
           />
         </div>
         <button
@@ -45,6 +83,10 @@ export function LoginForm() {
         >
           Login
         </button>
+
+        {errors.branchId?.message}
+        {errors.userName?.message}
+        {errors.password?.message}
       </form>
     </div>
   );
