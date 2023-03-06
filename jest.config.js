6bb12eaa9a0b34fe907eb0626db08cf13767c334
davidtaing <adavidtaing@gmail.com>
2023-03-06 22:51:11 +1100

    fix: TS paths not resolving for jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 const nextJest = require('next/jest')
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
 
 const createJestConfig = nextJest({
   dir: './',
@@ -8,6 +10,10 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 }
 
 module.exports = createJestConfig(customJestConfig)
