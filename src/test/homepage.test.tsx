bb275a7b02cf3f55b69d2dc6782491f5c7159740
davidtaing <adavidtaing@gmail.com>
2023-03-06 23:10:56 +1100

    test: home Login form
    Cases:
    - branch-id label is associated with branch-id input
    - email label is associated with email input
    - password label is associated with password input

--- src/test/homepage.test.tsx
+++ src/test/homepage.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Home from "@/pages";
 
@@ -34,5 +35,47 @@ it("renders Password label", () => {
   expect(passwordLabel).toBeTruthy();
 });
 
-  expect(branchIdLabel).toBeTruthy();
+test("branch-id label is associated with branch-id input", async () => {
+  const user = userEvent.setup();
+
+  render(<Home />);
+
+  const branchIdLabel = screen.getByLabelText(/^branch id$/i);
+
+  await user.click(branchIdLabel);
+  await user.keyboard("hello branch id");
+
+  const result = screen.getByDisplayValue(/^hello branch id$/i);
+
+  expect(result).toBeTruthy();
+});
+
+test("email label is associated with email input", async () => {
+  const user = userEvent.setup();
+
+  render(<Home />);
+
+  const emailLabel = screen.getByLabelText(/^email$/i);
+
+  await user.click(emailLabel);
+  await user.keyboard("hello email");
+
+  const result = screen.getByDisplayValue(/^hello email$/i);
+
+  expect(result).toBeTruthy();
+});
+
+test("password label is associated with password input", async () => {
+  const user = userEvent.setup();
+
+  render(<Home />);
+
+  const passwordLabel = screen.getByLabelText(/^password$/i);
+
+  await user.click(passwordLabel);
+  await user.keyboard("hello password");
+
+  const result = screen.getByDisplayValue(/^hello password$/i);
+
+  expect(result).toBeTruthy();
 });
