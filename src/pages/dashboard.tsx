a8862b1519b9a7c747334da736386c804287ee57
davidtaing <adavidtaing@gmail.com>
2023-03-07 21:28:58 +1100

    feat(Dashboard): redirect to home
    when user is not logged in
    
    Test Cases Added:
    - redirects the user to home if they are not logged in

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import Router from "next/router";
+
+import { useUserContext } from "@/modules/Auth/Users";
+
 function Dashboard() {
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (!user) Router.push("/");
+  }, [user]);
+
   return <div aria-label="Dashboard"></div>;
 }
 
--- src/test/dashboard.test.tsx
+++ src/test/dashboard.test.tsx
@@ -1,9 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, renderHook, screen } from "@testing-library/react";
+
+import { UserContextProvider } from "@/modules/Auth/Users";
 import Dashboard from "@/pages/dashboard";
 
+import { useRouter } from "next/router";
+import mockRouter from "next-router-mock";
+jest.mock("next/router", () => require("next-router-mock"));
+
 test("smoke test if it renders", () => {
-  render(<Dashboard />);
+  render(
+    <UserContextProvider>
+      <Dashboard />
+    </UserContextProvider>
+  );
   const dashboard = screen.getByLabelText(/^dashboard$/i);
 
   expect(dashboard).toBeTruthy();
+});
+
+it("redirects the user to home if they are not logged in", () => {
+  const { result } = renderHook(() => useRouter());
+
+  render(
+    <UserContextProvider>
+      <Dashboard />
+    </UserContextProvider>
+  );
+
+  expect(result.current).toMatchObject({ asPath: "/" });
 });
