c469261af1606fd4d223afae30534d585e1a6d1a
davidtaing <adavidtaing@gmail.com>
2023-03-07 20:57:50 +1100

    style: Login form input validation error messages

--- src/modules/Login/LoginForm.tsx
+++ src/modules/Login/LoginForm.tsx
@@ -46,12 +46,12 @@ export function LoginForm() {
   return (
     <div
       aria-label="Login Panel"
-      className="m-auto max-w-xl border border-slate-900 p-8"
+      className="m-auto w-full max-w-md border border-slate-900 p-8"
     >
       <h1 className="text-2xl font-medium">Login</h1>
       <form
         onSubmit={handleSubmit(onLoginFormSubmit)}
-        className="flex flex-col gap-8"
+        className="flex flex-col gap-2"
       >
         <div className="flex flex-col gap-2">
           <label htmlFor="branch-id" className="text-sm">
@@ -63,7 +63,9 @@ export function LoginForm() {
             {...register("branchId")}
             className="rounded-md border border-slate-900 px-4 py-2"
           />
-          <span className="text-red-500">{errors.branchId?.message}</span>
+          <span className="m-h-5 text-sm text-red-500">
+            {errors.branchId?.message ?? <span className="invisible">.</span>}
+          </span>
         </div>
 
         <div className="flex flex-col gap-2">
@@ -76,7 +78,9 @@ export function LoginForm() {
             {...register("userName")}
             className="rounded-md border border-slate-900 px-4 py-2"
           />
-          <span className="text-red-500">{errors.userName?.message}</span>
+          <span className="text-sm text-red-500">
+            {errors.userName?.message ?? <span className="invisible">.</span>}
+          </span>
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="password" className="text-sm">
@@ -88,11 +92,13 @@ export function LoginForm() {
             {...register("password")}
             className="rounded-md border border-slate-900 px-4 py-2"
           />
-          <span className="text-red-500">{errors.password?.message}</span>
+          <span className="text-sm text-red-500">
+            {errors.password?.message ?? <span className="invisible">.</span>}
+          </span>
         </div>
         <button
           type="submit"
-          className="rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 active:bg-blue-800"
+          className="mt-4 rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 active:bg-blue-800"
         >
           Login
         </button>
